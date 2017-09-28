ec4c4fe59431eec4e4e49f86413f0d8426d04918
yuankui <yuankui@youzan.com>
2017-09-28 10:56:19 +0800

    add css

--- src/main/java/com/yuankui/web/entity/Task.java
+++ src/main/java/com/yuankui/web/entity/Task.java
@@ -1,22 +1,36 @@
 package com.yuankui.web.entity;
 
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import lombok.Data;
 
 @Entity
 @Data
+@Table(uniqueConstraints = @UniqueConstraint(
+    name = "unique_name",
+    columnNames = {"name"}
+))
 public class Task {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
-  private Integer id;
+  private Long id;
   
-  @Id
   private String name;
-  private String body;
-
+  
+  private Integer minThreadNum;
+  private Integer maxThreadNum;
+  
+  @Column(columnDefinition = "TEXT")
+  private String inputConfig;
+  @Column(columnDefinition = "TEXT")
+  private String filterConfig;
+  @Column(columnDefinition = "TEXT")
+  private String outputConfig;
 }
